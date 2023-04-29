9f3b712f4186d59e6ea890c87a2e7ee498a11787
Rajiv75 <kraj12631@gmail.com>
2023-04-30 01:54:29 +0530

    added animation

--- js/script.js
+++ js/script.js
@@ -35,7 +35,7 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["MERN Full Stack Developer", "Graphic Desginer", " Figma Designer", "Freelancer","Youtuber","Wordpress developer"],
+        strings: ["MERN Full Stack Developer","Graphic Desginer", " Figma Designer", "Freelancer","Youtuber","Wordpress developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
